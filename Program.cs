d374b4feb5ebd60c499390310ef8d314620c078f
baseeee <13604740+baseeee@users.noreply.github.com>
2019-07-15 22:06:39 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,6 @@ class Tester
         Code c = new Code(input);
         Console.WriteLine("'{0}' Translates to '{1}'", c.GetInput(), c.GetTranslation());
         // Play() method is only supported on Windows
-        //code.Play();
+        //c.Play();
     }
 }
